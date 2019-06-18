2212a5380046143a1c8275ba6e128026124f9acd
garvin tan <garvintan07@gmail.com>
2019-06-18 22:54:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
-var playlist = {artist: songs}
+var playlist = {
+  Kanye: "Devil in a New Dress"
+  
+}
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song
+  
+  return playlist
+}
